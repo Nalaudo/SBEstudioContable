dfbe3445a934a733f314179291aa72545a93bcf7
Nicolas Balaudo <nicobalaudo@gmail.com>
2026-07-09 11:21:11 -0300

    fix: update AOS initialization parameters for improved animation performance

--- components/ui/AosInit.tsx
+++ components/ui/AosInit.tsx
@@ -6,9 +6,8 @@ import AOS from "aos";
 export default function AosInit() {
   useEffect(() => {
     AOS.init({
-      once: true, // La animación ocurre solo una vez al bajar
-      offset: 100, // Desplazamiento (en px) desde el elemento original
-      duration: 800, // Duración base de las animaciones
+      offset: 200, // Desplazamiento (en px) desde el elemento original
+      duration: 1000, // Duración base de las animaciones
       easing: "ease-out-cubic", // Curva de aceleración súper suave
     });
   }, []);
